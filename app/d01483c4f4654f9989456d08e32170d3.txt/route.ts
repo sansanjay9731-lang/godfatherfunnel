689122a40c2bca339e1377a80f577501e60745df
Apple <apple@Apples-MacBook-Pro-2.local>
2026-09-09 22:48:48 +0530

    fix: Serve IndexNow static txt key directly from public directory

--- app/d01483c4f4654f9989456d08e32170d3.txt/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return new NextResponse("d01483c4f4654f9989456d08e32170d3", {
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
-}
